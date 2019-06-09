81d82806a09ace53b28135af33fec64c963d2f08
Krzysztof Tomkow <krzysztof.tomkow@gmail.com>
2019-06-09 16:20:00 +0200

    Repetitions change

--- Text/tests.sh
+++ Text/tests.sh
@@ -6,18 +6,18 @@ cd ..
 
 filepath=~/Thesis/Text/files/150.txt
 common_name=150
-repetitions=10
+repetitions=1
 ./runTest.sh $filepath $common_name $repetitions
 wait
 
 filepath=~/Thesis/Text/files/text.txt
 common_name=text
-repetitions=10
+
 ./runTest.sh $filepath $common_name $repetitions
 wait
 
 filepath=~/Thesis/Text/files/large.txt
 common_name=large
-repetitions=10
+
 ./runTest.sh $filepath $common_name $repetitions
 wait
